d2a082c303effe1d12c62b210e7a88136914e3cb
Vova Potiichuk <volodymyr.potiichuk@prosapient.com>
2022-09-25 21:09:51 +0300

    feat: change files structure

--- challenges/easy/awaited.ts
+++ challenges/easy/awaited.ts
@@ -1,6 +1,6 @@
 type MyAwaited<T extends Promise<any>> = T extends Promise<infer P> ? P : never;
 
-type ExampleType = Promise<string>;
+type ExampleType = Promise<number>;
 
 type Result = MyAwaited<ExampleType>; // string
 
